d06426b5a790a75ef696b5654018f9202000b9a3
GuillaumePeixoto <guillaume.duarte78@gmail.com>
2026-08-25 17:02:15 +0200

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+
+    return (
+        <footer class="bg-[#1a1a1a] h-20 p-4 flex items-center justify-center">
+            <p className="text-lg text-white hover:text-(--yellow)">RezGame</p>
+        </footer>
+    )
+}
+
+export default Footer;
